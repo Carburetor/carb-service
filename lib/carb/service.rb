3abe6c6bf10c72a064d35af37696ef97c68ec302
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2016-12-06 19:45:20 -0800

    Passing basic service with call

--- lib/carb/service.rb
+++ lib/carb/service.rb
@@ -2,5 +2,8 @@ require "carb/service/version"
 
 module Carb
   module Service
+    def call(**args)
+      raise NotImplementedError
+    end
   end
 end
--- spec/carb/service_spec.rb
+++ spec/carb/service_spec.rb
@@ -1,7 +1,27 @@
 require "spec_helper"
+require "carb/service"
 
 describe Carb::Service do
+  before do
+    @klass = Class.new do
+      include Carb::Service
+    end
+    @service = @klass.new
+    @caller  = @klass.new
+
+    def @caller.call(**args)
+    end
+  end
+
   it "has a version number" do
     expect(Carb::Service::VERSION).to be_a String
+  end
+
+  it "raises not implemented error when called" do
+    expect{@service.()}.to raise_error NotImplementedError
+  end
+
+  it "accepts named args when called" do
+    expect{@service.(named: "arg")}.to raise_error NotImplementedError
   end
 end
